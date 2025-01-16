0752b7ad4968e00ef243feb7d3ea423a625c2084
Mathis Ballet <maballet@student.42lyon.fr>
2025-01-16 17:38:37 +0100

    restart

--- push_swap.c
+++ push_swap.c
@@ -6,68 +6,50 @@
 /*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:49:23 by maballet          #+#    #+#             */
-/*   Updated: 2025/01/09 17:40:46 by maballet         ###   ########lyon.fr   */
+/*   Updated: 2025/01/16 17:27:20 by maballet         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-stack   **push_swap(char **number)
+int   **push_swap(char **numbers, int argc)
 {
-	stack	**piles;
-	stack	pile_a;
-	
-	pile_a = stack_init(number);
-	piles[0][0] = pile_a;
-	return(piles);
+	int	i;
+	int	**pile;
+
+	i = 0;
+	pile[0] = malloc(sizeof(int*) * (argc - 1));
+	pile[1] = malloc(sizeof(int*) * (argc - 1));
+	while (numbers[i + 1] != NULL)
+	{
+		pile[0][i] = ft_atoi(numbers[i + 1]);
+		i++;
+	}
+	return(pile);
 }
 
-void    main(int argc, char **argv)
+int    main(int argc, char **argv)
 {
 	int	**result;     // 1 tab of tab for each stack.
 	int	i;
-	int	j;
+	int j;
 
 	j = 0;
-	result = push_swap(argv);
-	while (result[j][i])
+	i = 0;
+	result = push_swap(argv, argc);
+	while (result[i][j])
 	{
-		i = 0;
-		if (i == 0 && j == 1)
-			ft_printf("A\n_\n");
-		while (result[j][i])
+		j = 0;
+		if (i == 0)
+			ft_printf("A\n-\n");
+		while(result[i][j])
 		{
-			ft_printf("%d\n", result[j][i]);
-			i++;
+			ft_printf("%d\n", result[i][j]);
+			j++;
 		}
-		j++;
-		if (i == 0 && j == 2)
-			ft_printf("B\n_\n");
+		i++;
+		if (i == 1)
+			ft_printf("\nB\n-\n");
 	}
 	return(0);
-}
-
-int	ps_atoi(char *nptr)
-{
-	int		i;
-	int		sign;
-	long	result;
-
-	i = 0;
-	sign = 1;
-	result = 0;
-	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
-		i++;
-	if (nptr[i] == '+' || nptr[i] == '-')
-	{
-		if (nptr[i] == '-')
-			sign = -1;
-		i++;
-	}
-	while (nptr[i] >= '0' && nptr[i] <= '9')
-	{
-		result = result * 10 + (nptr[i] - '0');
-		i++;
-	}
-	return (result * sign);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,27 +6,16 @@
 /*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 19:58:49 by maballet          #+#    #+#             */
-/*   Updated: 2025/01/09 17:19:09 by maballet         ###   ########lyon.fr   */
+/*   Updated: 2025/01/16 17:18:29 by maballet         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include <unistd.h>
+# include "libft/inc/libft.h"
 
-typedef struct	stack
-{
-	void			*content;
-	struct stack	*next;
-}
-                stack;
+int   **push_swap(char **numbers, int argc);
 
-stack	**push_swap(char **number);
-stack	*new_node(void *number);
-void	pile_addback(stack **current_pile, stack *new);
-stack	stack_init(char **argv);
-int		ps_atoi(char *nptr);
-void	ps_lstclear(stack **lst, void (*del)(void *));
 
 #endif
